1e6cc669291cd44361918cd353b954152b34438f
zengqiyan <1214443162@qq.com>
2016-09-20 16:11:56 +0800

    版本2

--- src/com/zqy/sqllucene/lucenehandle/SelectHandle.java
+++ src/com/zqy/sqllucene/lucenehandle/SelectHandle.java
@@ -73,10 +73,10 @@ public class SelectHandle {
          return new IndexSearcher(new MultiReader(indexReaders));
      }
 	  /*
-	   * term为查询的最小单位，termQuery的查询字符串将作为一个整体，不会被分词
-	   * 指定field进行查询，termquery不能进行数字和日期的查询
-       * 日期的查询需要转成数字进行查询，
-       * 数字查询使用NumbericRangeQuery
+	   * term涓烘煡璇㈢殑鏈�灏忓崟浣嶏紝termQuery鐨勬煡璇㈠瓧绗︿覆灏嗕綔涓轰竴涓暣浣擄紝涓嶄細琚垎璇�
+	   * 鎸囧畾field杩涜鏌ヨ锛宼ermquery涓嶈兘杩涜鏁板瓧鍜屾棩鏈熺殑鏌ヨ
+       * 鏃ユ湡鐨勬煡璇㈤渶瑕佽浆鎴愭暟瀛楄繘琛屾煡璇紝
+       * 鏁板瓧鏌ヨ浣跨敤NumbericRangeQuery
 	     */	 
     public void termQuery(String columnName,Object text) {
         try {
@@ -91,7 +91,7 @@ public class SelectHandle {
             TermQuery termQuery = new TermQuery(term);
             System.out.println(termQuery);
             TopDocs topdocs = getSearcher().search(termQuery, sizes);
-            System.out.println("共找到" + topdocs.scoreDocs.length + ":条记录");
+            System.out.println("鍏辨壘鍒�" + topdocs.scoreDocs.length + ":鏉¤褰�");
             for (ScoreDoc scoreDocs : topdocs.scoreDocs) {
                 int documentId = scoreDocs.doc;
                 Document document = getSearcher().doc(documentId);
@@ -103,8 +103,8 @@ public class SelectHandle {
     }
     
     /*
-     * 使用parser可以替代query
-     * 如下相当于 钢铁  / 侠 使用 BooleanQuery的Must
+     * 浣跨敤parser鍙互鏇夸唬query
+     * 濡備笅鐩稿綋浜� 閽㈤搧  / 渚� 浣跨敤 BooleanQuery鐨凪ust
      */
 
     public void parser(String columnName,String text) {
@@ -115,7 +115,7 @@ public class SelectHandle {
             Query query = parser.parse(text);
             System.out.println("query:" + query);
             TopDocs topdocs = getSearcher().search(query, sizes);
-            System.out.println("共找到" + topdocs.scoreDocs.length + ":条记录");
+            System.out.println("鍏辨壘鍒�" + topdocs.scoreDocs.length + ":鏉¤褰�");
             for (ScoreDoc scoreDocs : topdocs.scoreDocs) {
                 int documentId = scoreDocs.doc;
                 Document document = getSearcher().doc(documentId);
@@ -129,7 +129,7 @@ public class SelectHandle {
     }
     
     /*
-     * parser相当于rangeQuery
+     * parser鐩稿綋浜巖angeQuery
      */
     public void rangeQueryParser(String columnName,float min ,float max) {
         try {
@@ -140,7 +140,7 @@ public class SelectHandle {
             Query query = parser.parse(queryString);
             System.out.println("query:" + query.toString());
             TopDocs topDocs = getSearcher().search(query, 10000);
-            System.out.println("共找到:" + topDocs.totalHits + "条记录");
+            System.out.println("鍏辨壘鍒�:" + topDocs.totalHits + "鏉¤褰�");
             for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
                 int docId = scoreDoc.doc;
                 Document document = getSearcher().doc(docId);
@@ -154,7 +154,7 @@ public class SelectHandle {
 
     }
     /*
-     * 前缀查询
+     * 鍓嶇紑鏌ヨ
      */
     
     public void prefixQuery(String columnName,String prefix) {
@@ -162,7 +162,7 @@ public class SelectHandle {
             Term prefixTerm = new Term(columnName, prefix);
             PrefixQuery query = new PrefixQuery(prefixTerm);
             TopDocs topDoc = getSearcher().search(query, sizes);
-            System.out.println("找到记录数:" + topDoc.totalHits);
+            System.out.println("鎵惧埌璁板綍鏁�:" + topDoc.totalHits);
             for (ScoreDoc scoreDoc : topDoc.scoreDocs) {
                 int docId = scoreDoc.doc;
                 Document document = getSearcher().doc(docId);
@@ -174,10 +174,10 @@ public class SelectHandle {
 
     }
     /*
-     * 短语查询
-     * 如下默认搜索为  雅钢铁
-     * 可以通过设置坡度（setSlop）来限定中间可以插入的字数
-     * 默认为0
+     * 鐭鏌ヨ
+     * 濡備笅榛樿鎼滅储涓�  闆呴挗閾�
+     * 鍙互閫氳繃璁剧疆鍧″害锛坰etSlop锛夋潵闄愬畾涓棿鍙互鎻掑叆鐨勫瓧鏁�
+     * 榛樿涓�0
      */
     
     public void phraseQuery(String columnName,int slop,String...texts) {
@@ -187,14 +187,14 @@ public class SelectHandle {
         		 Term term = new Term(columnName, texts[i]);
         		 phraseQuery.add(term);
         	}
-            //Term term1 = new Term("bookName", "雅");
-            //Term term2 = new Term("bookName", "钢铁");
+            //Term term1 = new Term("bookName", "闆�");
+            //Term term2 = new Term("bookName", "閽㈤搧");
             //phraseQuery.add(term1);
             //phraseQuery.add(term2);
             phraseQuery.setSlop(slop);
             System.out.println(phraseQuery.toString());
             TopDocs topDocs = getSearcher().search(phraseQuery, sizes);
-            System.out.println("找到记录数:" + topDocs.totalHits);
+            System.out.println("鎵惧埌璁板綍鏁�:" + topDocs.totalHits);
             for (ScoreDoc score : topDocs.scoreDocs) {
                 int docId = score.doc;
                 Document document = getSearcher().doc(docId);
@@ -205,16 +205,16 @@ public class SelectHandle {
         }
     }
     /*
-     * 多短语查询
-     * 查询结果为钢铁侠 或 钢铁情缘
+     * 澶氱煭璇煡璇�
+     * 鏌ヨ缁撴灉涓洪挗閾佷緺 鎴� 閽㈤搧鎯呯紭
      */
     
     
     public void multiPhraseQuery(String columnName,List<Object> textList){
         try {
-            /*String queryStr1 = "钢铁";
-            String queryStr2 = "侠";
-            String queryStr3 = "情缘";
+            /*String queryStr1 = "閽㈤搧";
+            String queryStr2 = "渚�";
+            String queryStr3 = "鎯呯紭";
             Term term1 = new Term("bookName", queryStr1);
             Term term2 = new Term("bookName",queryStr2);
             Term term3 = new Term("bookName",queryStr3);*/
@@ -242,7 +242,7 @@ public class SelectHandle {
             }
             multiPhraseQuery.setSlop(10);
             TopDocs topDocs = getSearcher().search(multiPhraseQuery,sizes);
-            System.out.println("找到记录数:"+topDocs.totalHits);
+            System.out.println("鎵惧埌璁板綍鏁�:"+topDocs.totalHits);
             for(ScoreDoc scoreDoc : topDocs.scoreDocs){
                 int docId = scoreDoc.doc;
                 Document document = getSearcher().doc(docId);
@@ -254,9 +254,9 @@ public class SelectHandle {
     
     }
     /*
-     * 模糊查询（相似度查询）
-     * 默认的相似度为0.5
-     * 在实例化FuzzyQuery时可以改变相似度
+     * 妯＄硦鏌ヨ锛堢浉浼煎害鏌ヨ锛�
+     * 榛樿鐨勭浉浼煎害涓�0.5
+     * 鍦ㄥ疄渚嬪寲FuzzyQuery鏃跺彲浠ユ敼鍙樼浉浼煎害
      */
     
     public void fuzzyQuery(String columnName,String text,int maxEdits){
@@ -264,7 +264,7 @@ public class SelectHandle {
             Term term = new Term(columnName,text);
             FuzzyQuery fuzzyQuery = new FuzzyQuery(term,maxEdits);
             TopDocs topDocs = getSearcher().search(fuzzyQuery, sizes);
-            System.out.println("找到记录数:"+topDocs.totalHits);
+            System.out.println("鎵惧埌璁板綍鏁�:"+topDocs.totalHits);
             for(ScoreDoc scoreDoc :topDocs.scoreDocs){
                 int docId = scoreDoc.doc;
                 Document document = getSearcher().doc(docId);
@@ -275,17 +275,17 @@ public class SelectHandle {
         } 
     }
     /*
-     * 通配符查询
+     * 閫氶厤绗︽煡璇�
      */
     
     
     public void wildcardQuery(String columnName,String text){
         try {
-            //String queryString = "?品*";
+            //String queryString = "?鍝�*";
             Term term = new Term(columnName,text);
             WildcardQuery wildcardQuery = new WildcardQuery(term);
             TopDocs topDocs = getSearcher().search(wildcardQuery, 10);
-            System.out.println("找到记录数："+topDocs.totalHits);
+            System.out.println("鎵惧埌璁板綍鏁帮細"+topDocs.totalHits);
             for(ScoreDoc scoreDoc : topDocs.scoreDocs){
                 int docId = scoreDoc.doc;
                 Document document = getSearcher().doc(docId);
@@ -297,18 +297,18 @@ public class SelectHandle {
     }
     
     /*
-     * 固定位置的词条查询
-     * eg:查询分词的第2个位置为大王的记录
+     * 鍥哄畾浣嶇疆鐨勮瘝鏉℃煡璇�
+     * eg:鏌ヨ鍒嗚瘝鐨勭2涓綅缃负澶х帇鐨勮褰�
      */
     
     public void spanFirstQuery(String columnName,String text,int end){
         try {
-            //String queryString = "大王";
+            //String queryString = "澶х帇";
             Term term = new Term(columnName,text);
             SpanTermQuery spanTermQuery = new SpanTermQuery(term);
             SpanFirstQuery spanFirstQuery = new SpanFirstQuery(spanTermQuery,end);
             TopDocs topDocs =  getSearcher().search(spanFirstQuery,sizes);
-            System.out.println("找到记录:"+topDocs.totalHits);
+            System.out.println("鎵惧埌璁板綍:"+topDocs.totalHits);
             for(ScoreDoc scoreDoc :topDocs.scoreDocs){
                 int docId = scoreDoc.doc;
                 Document document = getSearcher().doc(docId);
@@ -321,17 +321,17 @@ public class SelectHandle {
     
     /*
      * RegexQuery
-     * 正则查询，查询的字符串为一个正则表达式
+     * 姝ｅ垯鏌ヨ锛屾煡璇㈢殑瀛楃涓蹭负涓�涓鍒欒〃杈惧紡
      */
     
     
     public void regexQuery(String columnName,String regexText){
-        //String queryString = "^钢铁";
+        //String queryString = "^閽㈤搧";
         Term t = new Term(columnName,regexText);
         RegexQuery regexQuery = new RegexQuery(t);
         try {
             TopDocs topDocs = getSearcher().search(regexQuery, sizes);
-            System.out.println("找到数据："+topDocs.totalHits);
+            System.out.println("鎵惧埌鏁版嵁锛�"+topDocs.totalHits);
             for(ScoreDoc scoreDoc : topDocs.scoreDocs){
                 int docId = scoreDoc.doc;
                 Document document = getSearcher().doc(docId);
@@ -345,20 +345,20 @@ public class SelectHandle {
     
     
     /*
-     * parse类似于phraseQuery
-     * 使用parser时把一个查询字符串用双引号括起来表示该字符串不会被分词
+     * parse绫讳技浜巔hraseQuery
+     * 浣跨敤parser鏃舵妸涓�涓煡璇㈠瓧绗︿覆鐢ㄥ弻寮曞彿鎷捣鏉ヨ〃绀鸿瀛楃涓蹭笉浼氳鍒嗚瘝
      */
     
     
     public void parseLikePhraseQuery(String columnName,String parseText){
         try {
-            //String parseString = "\"钢铁侠\"装备";
+            //String parseString = "\"閽㈤搧渚燶"瑁呭";
             QueryParser parser = new QueryParser(Version.LUCENE_46, columnName,dataBaseDefaultConfig.getAnalyzer());
             parser.setDefaultOperator(Operator.AND);
             Query query = parser.parse(parseText);
             System.out.println("query:"+query);
             TopDocs topDocs = getSearcher().search(query, sizes);
-            System.out.println("找到记录数:"+topDocs.totalHits);
+            System.out.println("鎵惧埌璁板綍鏁�:"+topDocs.totalHits);
             for(ScoreDoc scoreDoc :topDocs.scoreDocs){
                 int docId = scoreDoc.doc;
                 Document document = getSearcher().doc(docId);
@@ -372,17 +372,17 @@ public class SelectHandle {
     }
     
     /*
-     * parser和fuzzyQuery
+     * parser鍜宖uzzyQuery
      */
     
     public void parseLikeFuzzyQuery(String columnName,String text){
         try {
-            //String parseStr = "钢铁侠~0.6";
+            //String parseStr = "閽㈤搧渚爚0.6";
             QueryParser parser = new QueryParser(Version.LUCENE_46,columnName,dataBaseDefaultConfig.getAnalyzer());
             parser.setDefaultOperator(Operator.AND);
             Query query = parser.parse(text);
             TopDocs topDocs = getSearcher().search(query, sizes);
-            System.out.println("找到记录数："+topDocs.totalHits);
+            System.out.println("鎵惧埌璁板綍鏁帮細"+topDocs.totalHits);
             for(ScoreDoc scoreDoc :topDocs.scoreDocs){
                 int docId = scoreDoc.doc;
                 Document document = getSearcher().doc(docId);
@@ -398,13 +398,13 @@ public class SelectHandle {
     }
     
     /*
-     * 多域查询
+     * 澶氬煙鏌ヨ
      */
     
     
     public void multiFieldQueryParser(String [] queries,String [] fields,BooleanClause.Occur [] booleanClauseOccur){
         try {
-            //String bookNameString = "钢铁是怎样炼成 的";
+            //String bookNameString = "閽㈤搧鏄�庢牱鐐兼垚 鐨�";
             //String bookPriceString = "[70 TO 80]";
             //String [] queries = new String []{bookNameString,bookPriceString};
             //String [] fields = {"bookName","bookPrice"};
@@ -413,7 +413,7 @@ public class SelectHandle {
             Query query = MultiFieldQueryParser.parse(Version.LUCENE_46, queries, fields,booleanClauseOccur,ikAnalyzer);
             System.out.println("query:"+query);
             TopDocs topDocs = getSearcher().search(query,sizes);
-            System.out.println("找到记录数:"+topDocs.totalHits);
+            System.out.println("鎵惧埌璁板綍鏁�:"+topDocs.totalHits);
             Highlighter highLighter = new Highlighter(new QueryScorer(query));
             for(ScoreDoc scoreDoc : topDocs.scoreDocs){
                 int docId = scoreDoc.doc;
@@ -434,7 +434,7 @@ public class SelectHandle {
     }
     public void multiFieldQueryParser(String text,String... columnNames){
         try {
-            //String bookNameString = "钢铁是怎样炼成 的";
+            //String bookNameString = "閽㈤搧鏄�庢牱鐐兼垚 鐨�";
             //String bookPriceString = "[70 TO 80]";
             //String [] queries = new String []{bookNameString,bookPriceString};
             //String [] fields = {"bookName","bookPrice"};
@@ -443,7 +443,7 @@ public class SelectHandle {
             System.out.println("query:"+queryParser);
             Query  query = queryParser.parse(text);
             TopDocs topDocs = getSearcher().search(queryParser.parse(text),sizes);
-            System.out.println("找到记录数:"+topDocs.totalHits);
+            System.out.println("鎵惧埌璁板綍鏁�:"+topDocs.totalHits);
             Highlighter highLighter = new Highlighter(new QueryScorer(query));
             resultHandle(topDocs);
         } catch (ParseException e) {
@@ -477,6 +477,6 @@ public class SelectHandle {
     	selectHandle.setQueryColumn(new String[]{"id"});
     	//selectHandle.termQuery("id", 1005L);
     	selectHandle.fuzzyQuery("id", "1007",1);
-        //selectHandle.multiFieldQueryParser("1007","id","title","content");
+        selectHandle.multiFieldQueryParser("1007","id","title","content");
 	}
 }
